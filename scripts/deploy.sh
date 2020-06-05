4b2c27f47f89af87d799a6a87e547e27e23e6d23
Khaazz <khaaz.dev@gmail.com>
2020-06-05 22:07:41 +0200

    oups

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,7 +20,6 @@ git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git commit -m "deploy"
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f  master
-# https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com:Khaaz/khaaz.github.io.git
+git push -f https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/Khaaz/khaaz.github.io.git
 
 cd -
