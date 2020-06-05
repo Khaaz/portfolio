29900457edc7188fdf458459a05e27318c83e62d
Khaazz <khaaz.dev@gmail.com>
2020-06-05 21:16:04 +0200

    try

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,6 @@
 # abort on errors
 set -e
 
-git config user.name "$GITHUB_ACTOR"
-git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
-
 # navigate into the build output directory
 cd dist
 
@@ -17,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Khaaz/khaaz.github.io.git master
+git push -f https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com:Khaaz/khaaz.github.io.git master
 
 cd -
